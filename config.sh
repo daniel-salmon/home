5815c282615a25cba1383945a3ba959aaa9e389f
daniel-niche <dsalmon@niche.com>
2019-12-26 06:50:13 -0500

    Remove references to 'bin' repo from config.sh

--- config.sh
+++ config.sh
@@ -19,33 +19,29 @@ rm -rf Qogir-theme
 # Create src directory under which I like to keep repos
 mkdir -p ~/src/github.com/daniel-salmon
 
+# Create personal bin / scripts folder
+mkdir -p ~/bin
+
 # Clone my repo containing all configurations
 cd ~/src/github.com/daniel-salmon
 git clone https://github.com/daniel-salmon/dotfiles.git
 cd dotfiles
 
-# bashrc
-cat .bashrc | tee -a ~/.bashrc
-
 # vimrc
 cp .vimrc ~/.vimrc
+
+# Copy personal bin to ~/bin
+cp -r bin ~/bin
 
 # Load my custom dconf profile
 dconf load dconf.txt
 
+# bashrc
+cat .bashrc | tee -a ~/.bashrc
+source ~/.bashrc
+
 # Remove Ubuntu dock
 sudo rm -rf /usr/share/gnome-shell/extensions/ubuntu-dock@ubuntu.com
-
-# Create personal bin / scripts folder
-mkdir -p ~/bin
-
-# Clone GitHub repo containing personal bin files and copy to ~/bin
-cd ~/src/github.com/daniel-salmon/
-git clone https://github.com/daniel-salmon/bin
-cp -r bin ~/bin
-
-# Source ~/.bashrc
-source ~/.bashrc
 
 # Git configurations
 git config --global user.email me@daniel-salmon.com
